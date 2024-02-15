2f140d41f78f59840646af03ff8cf03c9177c69f
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-02-15 12:22:50 +0700

    frrf

--- js/app.js
+++ js/app.js
@@ -5,7 +5,17 @@ $(document).ready(function () {
 
 
 //maen aos
-$("section p, .golongan .img").attr({ "data-aos": "fade-up", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+$("section p, section .pencapaian").attr({ "data-aos": "fade-up", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 $("section h3").attr({ "data-aos": "zoom-in", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" });
 
-$(".siapa .org").attr({ "data-aos": "fade-left", "data-aos-delay": "100", "data-aos-duration": "700", "data-aos-offset": "10", "data-aos-easing": "linear" });
+$("section .hp, section .pencapaian").attr({ "data-aos": "zoom-in", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$("section img").attr({ "data-aos": "zoom-in", "data-aos-delay": "100", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$("section .cont").attr({ "data-aos": "fade-in", "data-aos-delay": "500", "data-aos-duration": "500", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$("section .org-one").attr({ "data-aos": "fade-down", "data-aos-delay": "100", "data-aos-duration": "500", "data-aos-offset": "10", "data-aos-easing": "linear" });
+
+$("section .org-two").attr({ "data-aos": "fade-down", "data-aos-delay": "500", "data-aos-duration": "700", "data-aos-offset": "10", "data-aos-easing": "linear" });
+
+$("section .org-three").attr({ "data-aos": "fade-down", "data-aos-delay": "1000", "data-aos-duration": "1000", "data-aos-offset": "10", "data-aos-easing": "linear" });
